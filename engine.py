0cd433b76f6d2b7e8c199215f3c80fe2b247954e
Daniel Onyeakazi <do4009@hw.ac.uk>
2026-05-15 16:10:07 +0100

    imports

--- engine.py
+++ engine.py
@@ -1,6 +1,8 @@
 from datacleanse import DataCleaner
 import pandas as pd
 import numpy as np
+from scipy import stats
+import nashpy as nash
 import wbgapi as wb
 import json
 from sklearn.linear_model import LinearRegression 
@@ -12,8 +14,6 @@ warnings.filterwarnings('ignore')
 
 # DataEngine is a class for orchestrating the math and game theory analysis on the cleaned dataset. It serves as a higher-level interface to the DataCleaner and can be extended with additional methods for specific analyses or visualizations in the future.
 class DataEngine:
-    def __init__(self, data_source):
-        self.data_cleaner = DataCleaner(data_source)
-
-    def process_data(self):
-        self.data_cleaner.clean_data()
+    def __init__(self, df):
+        super().__init__(self, data_source=None)
+        self.df = df
